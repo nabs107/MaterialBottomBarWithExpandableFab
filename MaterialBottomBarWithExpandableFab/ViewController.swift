b0d9727c4f6f96e094f0636c4a387fcb5f4f995a
Nabin Shrestha <nabin.shrestha@f1soft.com>
2021-05-17 16:24:58 +0545

    Initial Commit

--- MaterialBottomBarWithExpandableFab/ViewController.swift
+++ MaterialBottomBarWithExpandableFab/ViewController.swift
@@ -6,12 +6,62 @@
 //
 
 import UIKit
+import MaterialComponents
+import LBTATools
+import JJFloatingActionButton
 
 class ViewController: UIViewController {
+    
+    fileprivate let actionButton = JJFloatingActionButton()
+    
+    lazy var containerView: UIView = UIView()
+    
+    lazy var bottomAppBarView: MDCBottomAppBarView = {
+        let bottomAppBarView = MDCBottomAppBarView()
+        bottomAppBarView.floatingButton.backgroundColor = .white
+        bottomAppBarView.floatingButton.tintColor = .white
+        bottomAppBarView.setFloatingButtonPosition(.trailing, animated: false)
+        return bottomAppBarView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(containerView)
+        containerView.fillSuperviewSafeAreaLayoutGuide()
+        
+        containerView.stack(
+            UIView(),
+            bottomAppBarView
+        )
+        
+        let firstItem = JJActionItem()
+        firstItem.titleLabel.text = "Phone"
+        firstItem.buttonImage = UIImage(named: "phone_unfill")
+        firstItem.buttonImageColor = .white
+        firstItem.titleLabel.backgroundColor = .purple
+        firstItem.buttonColor = .purple
+        firstItem.action = { item in
+            
+        }
+        
+        let secondItem = JJActionItem()
+        secondItem.titleLabel.text = "Video"
+        secondItem.buttonImage = UIImage(named: "video_unfill")
+        secondItem.buttonImageColor = .white
+        secondItem.titleLabel.backgroundColor = .purple
+        secondItem.buttonColor = .purple
+        secondItem.action = { item in
+            
+        }
+              
+        actionButton.buttonColor = .purple
+        actionButton.addItem(firstItem)
+        actionButton.addItem(secondItem)
+        
+        bottomAppBarView.floatingButton.addSubview(actionButton)
+        actionButton.fillSuperview()
+        actionButton.display(inViewController: self, bottomInset: 30, trailingInset: 36)
     }
 
 
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'MaterialBottomBarWithExpandableFab' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MaterialBottomBarWithExpandableFab
+  pod 'MaterialComponents/BottomAppBar'
+  pod 'LBTATools'
+  pod 'JJFloatingActionButton'
+
+end
